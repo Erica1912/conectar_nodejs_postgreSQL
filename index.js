bfb0a459afa120fc8e7911e37233039480a5ec69
Erica1912 <eri.ruiz@hotmail.com.ar>
2021-08-29 18:40:59 -0500

    creando el insert

--- index.js
+++ index.js
@@ -12,16 +12,23 @@ const { Client, Pool } = require("pg");
   const pool = new Pool(config);
 
   const getUser = async () => {
-    try{
-        const res = await pool.query('select * from public."T_USUARIOS"');
-        console.log(res.rows);
-        pool.end();
-    }catch{
-
-    }
+    try {
+      const res = await pool.query('select * from public."T_USUARIOS"');
+      console.log(res.rows);
+      pool.end();
+    } catch {}
   };
 
-  getUser();
+  const insertUsu = async () => {
+    const insUsu =
+      'INSERT INTO public."T_USUARIOS"("NOMBRE_APELLIDO", "CEDULA", "TELEFONO")VALUES($1,$2,$3)';
+    const value = ["erica ruiz", "156", "15"];
+    const res = await pool.query(insUsu, value);
+    console.log(res);
+    pool.end();
+  };
+  insertUsu();
+  //getUser();
 
   /* const getUser = async (pool) => {
     const { rows } = await pool.query('select * from public."T_USUARIOS"');
